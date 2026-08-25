dec9a57800e149696d8a81013c6504ba04d423cb
reyjustineee <magsinoreyjustine@gmail.com>
2026-08-25 12:49:43 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,22 @@
+# operators
+# + - Addition
+# - - Substraction
+# * - Multiplication
+# / - Division
+# // - Floor Division (round down)
+# ** - exponention (exponents)
+# % - modulus
+
+a = 24
+b = 12
+sum = a + b
+diff = a - b
+
+
+print("The sum of ",a,"plus ",b,"is ", sum)
+print("The sum of ",a,"minus ",b,"is ", diff)
+print("The sum of ",a,"times ",b,"is ", a*b)
+print("The sum of ",a,"divide ",b,"is ", a/b)
+print("The sum of ",a,"modulus ",b,"is ", a%b)
+print("The sum of ",a,"exponent ",b,"is ", a**b)
+print("The sum of ",a,"floor div ",b,"is ", a//b)
