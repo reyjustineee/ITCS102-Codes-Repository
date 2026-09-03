de064c210ba6cf8c09abf43dfaa1fa898e714649
reyjustineee <magsinoreyjustine@gmail.com>
2026-09-03 12:27:21 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,34 @@
+total = 0
+
+n1 = eval(input("Enter a number ---> "))
+total = total + n1
+#total1 = total + n1
+
+n1 = eval(input("Enter a number ---> "))
+total += n1
+
+n1 = eval(input("Enter a number ---> "))
+total += n1
+
+n1 = eval(input("Enter a number ---> "))
+total += n1
+
+n1 = eval(input("Enter a number ---> "))
+total += n1
+
+n1 = eval(input("Enter a number ---> "))
+total += n1
+
+n1 = eval(input("Enter a number ---> "))
+total -= n1
+
+n1 = eval(input("Enter a number ---> "))
+total /= n1
+
+n1 = eval(input("Enter a number ---> "))
+total *= n1
+
+n1 = eval(input("Enter a number ---> "))
+total = total + n1
+
+print("The sum is", total)
---
+++ activity8.py
@@ -0,0 +1,14 @@
+#concentration = combining strings
+
+hobbies = "" #blank string
+
+h = input("What are your hobbies ---> ")
+hobbies += h + ", "
+
+h = input("What else ---> ")
+hobbies += h + ", "
+
+h = input("What else ---> ")
+hobbies += h + ", "
+
+print("So, my hobbies are", hobbies)
