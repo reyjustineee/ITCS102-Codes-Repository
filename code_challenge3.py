994e9d077a0c93ee0af342319a7e0eb8ec3587ba
reyjustineee <magsinoreyjustine@gmail.com>
2026-09-11 14:34:01 +0800

    Add files via upload

--- code_challenge3.py
+++ code_challenge3.py
@@ -2,39 +2,32 @@
 
 #input
 
-print("_____________________________________\n")
+print("_____________________________________")
 send_name = input("Enter Name ---> ")
 type_of_item = input("Type of Item ---> ")
-is_Fragile = bool(input("Fragile(True/False)? ---> "))
+is_Fragile = bool(input("Fragile?(Enter \"Yes\" of yes, enter \"No\" if no) ---> ") == "Yes")
 weight = float(input("Weight (in kg) ---> "))
 distance = float(input("Distance (in km) ---- > "))
-is_Express = bool(input("Express(True/False)? ---> "))
-is_International = bool(input("International(True/False)? ---> "))
+is_Express = bool(input("Express (Enter \"Yes\" of yes, enter \"No\" if no) ---> ") == "Yes")
+is_International = bool(input("International (Enter \"Yes\" of yes, enter \"No\" if no) ---> ") == "Yes")
 
-#calculationg base cost
+#calculation
 
 base_cost = (weight * 2.5) + (distance * 0.15)
-
-#free shipping
 
 if weight <= 2.0 and distance <= 100 and not is_Express and not is_International:
 	total = 0
 
-#internaional express
 elif is_International and is_Express:
-	total = (base_cost * 1.40) + 50
+	total = (base_cost * 1.4) + 50
 
-#express or heavy international
-elif weight > 20 and is_Express or is_International:
-	total = (base_cost * 1.20) + 25
+elif (is_Express or is_International) and weight > 20:
+	total = (base_cost * 1.2) + 25
 
-#oversized
-elif distance > 1000 or weight > 30:
-	total = (base_cost) + 30
+elif weight > 30 or distance > 1000:
+	total = base_cost + 30
 else:
 	total = base_cost
-
-shipping_fee = total - base_cost
 
 print("_____________________________________\n")
 print("COMPANY")
@@ -46,6 +39,5 @@ print("DISTANCE: ", distance)
 print("EXPRESS: ", is_Express)
 print("INTERNATIONAL: ", is_International)
 print("\nTOTAL: ", total)
-print("The total shipping cost is: ", shipping_fee)
 print("_____________________________________")
 
